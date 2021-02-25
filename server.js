189bac442804dffa3b73d1af097c620be6fb88d0
andrew <ap@ampiusa.com>
2021-02-25 10:35:02 -0800

    get requests

--- server.js
+++ server.js
@@ -1,16 +1,32 @@
 const express = require('express');
 const app = express();
-app.listen(3001, () =>
-{
-    console.log('API server now on port 3001!');
-})
+const path = require('path');
 const db  = require('./data/db')
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
-app.get('/api/db', (req,res) => {
-    let results = db
-    console.log(req.query)
-    res.json(results)
+// LINK HTML PATHS//
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
+app.get('/notes', (req, res)=> {
+    res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+});
+
+
+
+
+
+
+
+
+
+// ================LISTENER===================//
+
+app.listen(3001, () =>
+{
+    console.log('API server now on port 3001!');
+})
