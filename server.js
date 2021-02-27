fb7fe0e4004b8d80811d0b765b89427954aaa1c5
andrew <ap@ampiusa.com>
2021-02-26 16:12:40 -0800

    added delete

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 // REQUIRED//
 const fs = require('fs');
 const express = require('express');
-const  { notesArray }  = require('./data/notes.json')
+const  { notes }  = require('./data/notes.json')
 const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 3001;
@@ -16,18 +16,41 @@ app.use(express.json())
 // =========================================//
 
 
-function createNewNote(body, notesArray) {
+// FUNCTIONS//
+
+function createNewNote(body, notes) {
     const note = body 
-    notesArray.push(note)
+    notes.push(note)
     fs.writeFileSync(
       path.join(__dirname, './data/notes.json'),
-      JSON.stringify({notesArray}, null, 2)
+      JSON.stringify({notes}, null, 2)
   );   
    
     return note
 };
 
-// LINK HTML PATHS//
+
+function deleteNotesUpdate(id, notes){
+  const deleteID = id
+  const updatedArray =  notes.filter(del => del.id != deleteID);
+  console.log('updated array', updatedArray);
+  notes=updatedArray
+  
+  console.log('Updated Notes Object', notes);
+  fs.writeFileSync(
+    path.join(__dirname, './data/notes.json'),
+    JSON.stringify({notes}, null , 2)
+);   
+
+  return updatedArray
+  
+}
+
+
+// =========================================//
+
+
+// LINK HTML PATHS and GET Request//
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
     console.log(notes);    
@@ -41,20 +64,34 @@ app.get('/api/notes', (req,res)=>{
     res.sendFile(path.join(__dirname, 'data', 'notes.json'));
  });
 
+
+// ========================================//
+
+
+// POST Request //
  app.post('/api/notes', (req, res) => {
     req.body.id = uniqID;
-    const note = createNewNote(req.body, notesArray);
-    console.log(req.body);    
+    const note = createNewNote(req.body, notes);
+    console.log(req.body);   
+  
+ 
 
-    res.json(req.body); 
-    
+    res.json(req.body);     
  });
+ 
+ //===========================================//
+
+//  DELETE Request //
+app.delete('/api/notes/:id', (req, res)=>{
+  const getID = req.params.id;  
+  const deleteNote = deleteNotesUpdate(getID, notes)
+  
+  res.json(req.params.id);
+});
 
 
 // Need to add event listener and add to save button to call//
  const uniqID = uniqid()
-
-
 
 // ================LISTENER===================//
 
